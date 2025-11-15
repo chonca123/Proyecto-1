e38ce6b9dabe77176b8876bcea5540681f5bc55f
Benjamin Concha <116835119+chonca123@users.noreply.github.com>
2025-11-15 00:54:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct SistemaVotacion {
+    struct NodoEleccion *Eleccionee;
+};
+
+struct NodoElectoral {
+  struct Elecciones *Elecciones;
+  struct NodoElectoral *sig;
+};
+
+struct Elecciones {
+    struct TodosCandidatos *Candidatos;
+    struct NodoMesa *ant;
+};
+
+struct TodosCandidatos {
+  struct Candidato*Candidato;
+};
+
+struct Candidato{
+  int Rut[12];
+  char NombreCandidato[256];
+  
+}
+
+struct NodoMesa{
+  struct Mesa*Mesa;
+  struct NodoMesa*sig;
+};
+
+struct Mesa{
+  int Mesa;
+  char Direccion[256];
+  struct NodoVotante*votantes;  
+};
+
+ struct NodoVotante{
+  struct Votante;
+  struct nodo_imputado *izq;
+  struct nodo_imputado *der;
+};
+
+struct votante{
+  struct Candidato*Candidatos;
+
+}
+  
