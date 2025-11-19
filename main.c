1a071b979fecab79f809772d4c6699c4151244d8
IgnacioRojas182 <ignacio.rojas.s@mail.pucv.cl>
2025-11-19 01:49:55 -0300

    Define structs for voting system and related entities
    Added structs for voting system, candidates, and voters.

--- main.c
+++ main.c
@@ -115,3 +115,97 @@ void IngresarDatosVotacion(struct SistemaVotacion *sistema) {
 
 
 
+
+//propuesta de structs con cambios
+//con variables de nombre distinto
+
+struct SistemaVotacion 
+{
+    struct NodoEleccion *elecciones;
+    char   *convocatoria;            //la convocatoria tiene el nombre de lo que se va a realizar
+    struct Fecha registroCandidaturas;    //se agrega la fecha del registro
+    struct Fecha FechaInicioCampana;
+    struct Fecha FechaVotacion;
+};
+
+struct Fecha 
+{
+    int dia;
+    int mes;
+    int anio;
+};
+
+struct NodoEleccion 
+{
+    struct Eleccion *datosEleccion;
+    struct NodoEleccion *sig;
+};
+
+struct Eleccion 
+{
+    int NumeroVuelta;          //es un entero no un arreglo
+    struct TodosCandidatos *listaCandidatos;
+    struct NodoMesa *listaMesas;
+};
+
+struct TodosCandidatos 
+{
+    struct Candidato **Candidatos;    //** porque es un arreglo de punteros a candidatos
+    int totalCandidatos;            //se agrega el largo del array
+};
+
+struct Candidato
+{
+    char Rut[15];                //el rut es un char
+    char *NombreCandidato;
+    char Nacionalidad[25];
+    int  edad;                  //edad es un numero
+    char *PartidoPolitico;
+    char *ProgramaGobierno;
+    int  delitos;                //variable para confirmar si tiene delitos o no
+};
+
+struct NodoMesa
+{
+    struct Mesa *datosMesa;
+    struct NodoMesa *sig;
+};
+
+struct Vocales
+{
+    char *nombre;
+    int edad;
+    char rut[15];
+};
+
+struct NodoVocal
+{
+    struct Vocales *datosVocal;
+    struct NodoVocal *sig;
+};
+
+struct Mesa
+{
+    struct NodoVocal *listaVocales;          //lista interna que contiene los vocales de la mesa, porque pueden haber varios
+    struct NodoVotante *votantes;  
+};
+
+ struct NodoVotante
+ {
+    struct Votante *datosVotante;
+    struct NodoVotante *izq, *der;
+};
+
+struct Votante
+{
+    struct Candidato *voto;
+    char *Nombre;
+    int  Edad;               //edad es un numero
+    char Nacionalidad[25];
+    char rut[15];
+    char paisResidencia[25];
+};
+
+//el padron mesa no va, porque corresponden a todos los votantes habilitados a votar, y esto es el arbol
+//uso de char estaticos donde el tamaño varia mucho y no se conoce
+//usar strlen para calcular el tamaño exacto del ingreso dinamico al agregar, se escanenan con auxiliares antes de todo
